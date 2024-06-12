8e9252ac14c70cb4ad82dc8e6eef2bb12139342c
test <leejeayoung875@gmail.com>
2024-06-12 11:25:41 +0900

    생성자 instanceof 사용

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-const basicData=() =>{
-  return{
-    name:"test",
-    age:20,
-    job:"미정"
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
   }
-}
-class BasicData{
-  constructor(name,age,job){
-    this.name=name;
-    this.age=age;
-    this.job=job;
+
+  set name(nameValue) {
+    if (typeof nameValue === "string") {
+      this._name = nameValue;
+    } else {
+      console.error("x");
+    }
+  }
+
+  set age(ageValue) {
+    if (typeof ageValue === "number") {
+      this._age = ageValue;
+    } else {
+      console.error("x");
+    }
+  }
+
+  set job(jobValue) {
+    if (typeof jobValue === "string") {
+      this._job = jobValue;
+    } else {
+      console.error("x");
+    }
   }
 }
 
-const commponent =()=>{  
-  let obj=basicData();
-  const decision=()=>{};
-  return`<html></html>`
-}
+const component = () => {
+  let obj = new BasicData("test", 20, "미정");
+  console.log(obj);
+  const decision = () => {
+    if (obj instanceof BasicData) {
+      return `<h1>${obj._name}</h1>`;
+    } else {
+      throw new Error("Error");
+    }
+  };
+  return decision();
+};
+console.log(component())
+component()
+
+
